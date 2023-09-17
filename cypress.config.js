f93225f3c3b846d73001492cc74994257cbe7d53
sanavila <rute.leone@hotmail.com>
2023-09-17 09:35:50 -0300

    update cypress version

--- cypress.config.js
+++ cypress.config.js
@@ -3,8 +3,15 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   // implement node event listeners here
   baseUrl: "http://lojaebac.ebaconline.art.br/",
+
   env: {
     EMAIL_EBAC: "aluno_ebac@teste.com",
     SENHA_VALIDA: "teste@teste.com",
+  },
+
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
   },
 });
